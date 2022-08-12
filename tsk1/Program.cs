d2cea155d3f9ccb1387cd6f885118aa961680681
Maxim <you@example.com>
2022-08-12 22:37:41 +0300

    com1

---
+++ tsk1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter a number from 100 to 999");
+int number0 = int.Parse(Console.ReadLine());
+int number1 = number0 % 10;
+int number2 = number0 / 100;
+int number3 = number0 % 100;
+int number4 = number3 / 10;
+int result = number2 * 10 + number1;
+Console.WriteLine($"Number {result} from {number0}, second digit was {number4}");
