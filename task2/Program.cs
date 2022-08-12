79455bf752d71526814c7752a625397842be7a60
Maxim <you@example.com>
2022-08-12 22:41:02 +0300

    com2

---
+++ task2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter a number");
+int number0 = int.Parse(Console.ReadLine());
+Console.WriteLine(number0 / 100 == 0 ? "EROR!!!!!!!!!!!!!!!!!.Please enter a three or more digit number" : number0);
+int number1 = number0 / 100;
+int number2 = number1 % 10;
+
+Console.WriteLine($" The third digit of the number is: {number2}");
